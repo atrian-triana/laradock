562c26a2898368a86e2063d99147ab6538dab394
adrian roberto triana miranda <atrian.triana@gmail.com>
2024-02-14 16:26:46 -0500

    cambios iniciales

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -7,6 +7,13 @@ if [ ! -f /etc/nginx/ssl/default.crt ]; then
     chmod 644 /etc/nginx/ssl/default.key
 fi
 
+if [ ! -f /etc/nginx/ssl/nuwwetest.org.crt ]; then
+    openssl genrsa -out "/etc/nginx/ssl/nuwwetest.org.key" 2048
+    openssl req -new -key "/etc/nginx/ssl/nuwwetest.org.key" -out "/etc/nginx/ssl/nuwwetest.org.csr" -subj "/CN=nuwwetest.org/O=nuwwetest.org/C=UK"
+    openssl x509 -req -days 365 -in "/etc/nginx/ssl/nuwwetest.org.csr" -signkey "/etc/nginx/ssl/nuwwetest.org.key" -out "/etc/nginx/ssl/nuwwetest.org.crt"
+    chmod 644 /etc/nginx/ssl/nuwwetest.org.key
+fi
+
 # Start crond in background
 crond -l 2 -b
 
